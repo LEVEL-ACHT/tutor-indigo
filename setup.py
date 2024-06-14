b7382da91616cef2585d5571f0b85a2173ccbd06
Faizan Nauman Shaikh <140354659+faizanns@users.noreply.github.com>
2024-06-14 08:03:30 +0200

    Update setup.py
    plugin name change from indigo to indigo-level-acht

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     python_requires=">=3.8",
     install_requires=["tutor>=17.0.0,<18.0.0", "tutor-mfe>=17.0.0,<18.0.0"],
     extras_require={"dev": "tutor[dev]>=17.0.0,<18.0.0"},
-    entry_points={"tutor.plugin.v1": ["indigo = tutorindigo.plugin"]},
+    entry_points={"tutor.plugin.v1": ["indigo-level-acht = tutorindigo.plugin"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
